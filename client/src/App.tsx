dde03a2e1bf8e29c27dc37bcb07e841f1c9228b4
toxxicjames <ibezim.dev@gmail.com>
2024-06-21 23:28:49 +0200

    using getstream.io

---
+++ client/src/App.tsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+import type { User, Channel as StreamChannel } from 'stream-chat';
+import { useCreateChatClient, Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+
+const apiKey = 'w5d9w2kwrtj8';
+const userId = '1320026';
+const userName = 'james';
+const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTMyMDAyNiJ9.Zm8SkoTAe8iVRD097rgZTyxTHUV6lLyYPlluVFhYOnA';
+
+const user: User = {
+  id: userId,
+  name: userName,
+  image: `https://getstream.io/random_png/?name=${userName}`,
+};
+
+const App = () => {
+  const [channel, setChannel] = useState<StreamChannel>();
+  const client = useCreateChatClient({
+    apiKey,
+    tokenOrProvider: userToken,
+    userData: user,
+  });
+
+  useEffect(() => {
+    if (!client) return;
+
+    const channel = client.channel('messaging', 'custom_channel_id', {
+      image: 'https://getstream.io/random_png/?name=react',
+      name: 'Talk about React',
+      members: [userId],
+    });
+
+    setChannel(channel);
+  }, [client]);
+
+  if (!client) return <div>Setting up client & connection...</div>;
+
+  return (
+    <Chat client={client}>
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+};
+
+export default App;
